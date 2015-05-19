00cf9861b73bf3d463402269e45bb6bbac5ebd2e
h <dangerousElectrician@gmail.com>
2015-05-18 23:51:41 -0500

    removed arduino motor driver stuff

--- EverGimbal.ino
+++ EverGimbal.ino
@@ -41,10 +41,11 @@ THE SOFTWARE.
 ===============================================
 */
 
-#define MOTOR_POLES 7
+#define MOTOR_POLES 4
+#define SERIAL_PORT Serial1
 
-#include "BLcontroller.h"         // Motor Movement Functions and Timer Config
-
+//#include "BLcontroller.h"         // Motor Movement Functions and Timer Config
+#include "TeensyBrushless.h"
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
@@ -139,19 +140,15 @@ void dmpDataReady() {
 // ================================================================
 
 void setup() {    
-    // Init BL Controller
-    initBlController();
-    // Init Sinus Arrays
-    initMotorStuff();
-    // switch off PWM Power
-    motorPowerOff();
-  
+    // configure LED for output
+    calcSinusArray();
+
 
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
-    Serial.begin(9600);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    SERIAL_PORT.begin(9600);
+    //while (!SERIAL_PORT); // wait for Leonardo enumeration, others continue immediately
   
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -162,45 +159,48 @@ void setup() {
     #endif
 
     // initialize device
-    //Serial.println(F("Initializing I2C devices..."));
+    //SERIAL_PORT.println(F("Initializing I2C devices..."));
     mpu.initialize();
-
     // verify connection
-    //Serial.println(F("Testing device connections..."));
-    //Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+    //SERIAL_PORT.println(F("Testing device connections..."));
+    //SERIAL_PORT.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // wait for ready
-    //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    //while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-
+    //SERIAL_PORT.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (SERIAL_PORT.available() && SERIAL_PORT.read()); // empty buffer
+    //while (!SERIAL_PORT.available());                 // wait for data
+    while (SERIAL_PORT.available() && SERIAL_PORT.read()); // empty buffer again
     // load and configure the DMP
-    //Serial.println(F("Initializing DMP..."));
+    //SERIAL_PORT.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
 
+//    mpu.setXGyroOffset(-29);
+//    mpu.setYGyroOffset(-68);
+//    mpu.setZGyroOffset(51);
+//    
+//    mpu.setXAccelOffset(-3261);
+//    mpu.setYAccelOffset(-35);
+//    mpu.setZAccelOffset(1853);
+    
     mpu.setXGyroOffset(-29);
     mpu.setYGyroOffset(-68);
     mpu.setZGyroOffset(51);
-    
-    mpu.setXAccelOffset(-3261);
-    mpu.setYAccelOffset(-35);
-    mpu.setZAccelOffset(1853);
+    mpu.setZAccelOffset(1853); // 1688 factory default for my test chip
 
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
-        //Serial.println(F("Enabling DMP..."));
+        //SERIAL_PORT.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
         // enable Arduino interrupt detection
-        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        pinMode(2, INPUT);
-        attachInterrupt(2, dmpDataReady, RISING);
+        //SERIAL_PORT.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        pinMode(20, INPUT);
+        attachInterrupt(20, dmpDataReady, RISING);
         mpuIntStatus = mpu.getIntStatus();
 
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        //Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        //SERIAL_PORT.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
 
         // get expected DMP packet size for later comparison
@@ -210,27 +210,26 @@ void setup() {
         // 1 = initial memory load failed
         // 2 = DMP configuration updates failed
         // (if it's going to break, usually the code will be 1)
-        //Serial.print(F("DMP Initialization failed (code "));
-        //Serial.print(devStatus);
-        //Serial.println(F(")"));
+        //SERIAL_PORT.print(F("DMP Initialization failed (code "));
+        //SERIAL_PORT.print(devStatus);
+        //SERIAL_PORT.println(F(")"));
     }
-
-    // configure LED for output
+  
     pinMode(LED_PIN, OUTPUT);
-
 
 }
 
 int16_t ax, ay, az;
 int power = 0;
-float scale = 10*256*MOTOR_POLES/(float)360; //Had a few issues here when the math was being done as ints and then cast to float instead of doing the math in float
+float scale = 256*MOTOR_POLES/(float)360; //Had a few issues here when the math was being done as ints and then cast to float instead of doing the math in float
 float Input = 0;
+TeensyBrushless Mot(23,22,21,90);
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 void loop() {
     // if programming failed, don't try to do anything
-    if (!dmpReady) return;
+    //if (!dmpReady) return;
     int pos = 0;
     while (!mpuInterrupt && fifoCount < packetSize) {
         // other program behavior stuff here
@@ -245,36 +244,41 @@ void loop() {
         // .
         Input = ((euler[0] * 180/M_PI)+180)*scale;
         pos = (int) (Input);
-        MoveMotorPosSpeed(1, pos, power);
-          
-        //Serial.println("mainloop");
-        if(Serial.available()) {
-          switch(Serial.read()) {
+        Mot.MoveMotorPosSpeed(pos);
+        Mot.updateMotor();
+        //SERIAL_PORT.println("mainloop");
+        if(SERIAL_PORT.available()) {
+          switch(SERIAL_PORT.read()) {
             case 'a':              
-              Serial.println(ax);
-              //Serial.print("\n");
-              Serial.println(ay);
-              //Serial.print("\n");
-              Serial.println(az);
+              SERIAL_PORT.println(ax);
+              SERIAL_PORT.println(ay);
+              SERIAL_PORT.println(az);
               break;
               
             case 'd':
-              power = 0;
+              Mot.disable();
               break;
             case 'f':
-              power =150;
+              Mot.enable();
               break;
               
             case 'q':
+              SERIAL_PORT.println("wait 2 sec");
               delay(2000);
-              Serial.println("here");
+              break;
+            case 'w':
+              for(int i = 0; i < 256*4; i++)
+              {
+                Mot.MoveMotorPosSpeed(i);
+                Mot.updateMotor();
+                delayMicroseconds(500);
+              }
               break;
           }
         }
 
     }
 
-    //delay(10); //ugly hack to avoid overflowing the FIFO
     
     
     // reset interrupt flag and get INT_STATUS byte
@@ -287,13 +291,13 @@ void loop() {
     // check for overflow (this should never happen unless our code is too inefficient)
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
         // reset so we can continue cleanly
-        Serial.println(F("1 FIFO overflow!"));
-        Serial.println(mpuIntStatus);
-        Serial.println(fifoCount);
+        //SERIAL_PORT.println(F("1 FIFO overflow!"));
+        //SERIAL_PORT.println(mpuIntStatus);
+        //SERIAL_PORT.println(fifoCount);
         mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-        Serial.println(mpuIntStatus);
-        Serial.println(fifoCount);
+        SERIAL_PORT.println(F("FIFO overflow!"));
+        //SERIAL_PORT.println(mpuIntStatus);
+        //SERIAL_PORT.println(fifoCount);
 
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
@@ -312,21 +316,21 @@ void loop() {
         // display Euler angles in degrees
         mpu.dmpGetQuaternion(&q, fifoBuffer);
         mpu.dmpGetEuler(euler, &q);
-        //Serial.print("euler\t");
-        //Serial.print(euler[0] * 180/M_PI+180);
-        //Serial.print("\t");
-        //Serial.print(euler[1] * 180/M_PI);
-        //Serial.print("\t");
-        //Serial.print(euler[2] * 180/M_PI);
-        //Serial.print("\t");
-        //Serial.print(Input);
-        //Serial.print("\t");
-        //Serial.println(pos & 0xff);
+        //SERIAL_PORT.print("euler\t");
+        //SERIAL_PORT.print(euler[0] * 180/M_PI+180);
+        //SERIAL_PORT.print("\t");
+        //SERIAL_PORT.print(euler[1] * 180/M_PI);
+        //SERIAL_PORT.print("\t");
+        //SERIAL_PORT.print(euler[2] * 180/M_PI);
+        //SERIAL_PORT.print("\t");
+        //SERIAL_PORT.print(Input);
+        //SERIAL_PORT.print("\t");
+        //SERIAL_PORT.println(pos & 0xff);
 
 
 
         // blink LED to indicate activity
-        //blinkState = !blinkState;
-        //digitalWrite(LED_PIN, blinkState);
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
     }
 }
